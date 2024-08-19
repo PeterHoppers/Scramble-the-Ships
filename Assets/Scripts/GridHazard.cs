22ef29510afb44ec444d30c9906e5f2113cdac06
Peter Hoppe <peterhoppe341@gmail.com>
2024-08-19 15:55:29 -0500

    Wall Broken Into Children
    - each part of the wall can now be shot seperately

--- Assets/Scripts/GridHazard.cs
+++ Assets/Scripts/GridHazard.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridHazard : GridObject
 {
+    public override void SetupObject(GameManager manager, SpawnSystem system, Tile startingTile)
+    {
+        var childrenObjects = GetComponentsInChildren<GridObject>().Skip(1).ToArray(); //get components in children, for some reason, returns the parent object
+
+        if (childrenObjects.Length > 0)
+        {
+            foreach (var child in childrenObjects)
+            {
+                child.SetupObject(manager, system, startingTile);
+            }
+        }
+        else
+        { 
+            base.SetupObject(manager, system, startingTile);
+        }        
+    }
+
     protected override void PerformInteraction(Collider2D collision)
     {
         if (!collision.TryGetComponent<GridObject>(out var collidedPreviewable))
