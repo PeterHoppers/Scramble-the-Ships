7d704f4bda034e36b66f060fb0e3553390678694
Peter Hoppe <peterhoppe341@gmail.com>
2024-09-20 11:30:35 -0500

    Dialogue Can Now Mention Who is Talking

--- Assets/ScriptableObjects/Scripts/DialogueNode.cs
+++ Assets/ScriptableObjects/Scripts/DialogueNode.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [System.Serializable]
 public struct DialogueNode
 {
+    public DialogueSpeaker textSpeaker;
     [TextArea(2, 4)]
     public string textToDisplay;
+}
+
+public enum DialogueSpeaker
+{
+    Control,
+    Player,
+    Alien
 }
